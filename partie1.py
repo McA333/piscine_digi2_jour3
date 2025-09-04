37bef2025f7ac81d953c1ebbdc25c266e68c384c
Marie Caron <marie.caron@epitech.eu>
2025-09-04 14:18:30 +0200

    feat: creation print url json.

--- partie1.py
+++ partie1.py
@@ -4,6 +4,9 @@ def get_requests(url) :
 
     url = requests.get(url)
     print(url.status_code)
-    #print(url.text)
+    print(url.json())
 
-#get_requests(input("Entrer l'URL du site : "))
+#def get_countries_info(country_code, info) :
+
+
+get_requests(input("Entrer l'URL du site : "))
